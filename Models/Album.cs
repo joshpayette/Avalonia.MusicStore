bba6227d204d5897fc5f29e02efcba4d0e396e72
Josh Payette <hello@joshpayette.dev>
2023-10-14 14:44:12 -0400

    Initial commit

---
+++ Models/Album.cs
@@ -0,0 +1,51 @@
+﻿using iTunesSearch.Library;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Avalonia.MusicStore.Models
+{
+    public class Album
+    {
+        private static iTunesSearchManager s_SearchManager = new();
+
+        private static HttpClient s_httpClient = new();
+        private string CachePath => $"./Cache/{Artist} - {Title}";
+
+        public async Task<Stream> LoadCoverBitmapAsync()
+        {
+            if (File.Exists(CachePath + ".bmp"))
+            {
+                return File.OpenRead(CachePath + ".bmp");
+            }
+            else
+            {
+                var data = await s_httpClient.GetByteArrayAsync(CoverUrl);
+                return new MemoryStream(data);
+            }
+        }
+
+        public Album(string artist, string title, string coverUrl)
+        {
+            Artist = artist;
+            Title = title;
+            CoverUrl = coverUrl;
+        }
+
+        public static async Task<IEnumerable<Album>> SearchAsync(string searchTerm)
+        {
+            var query = await s_SearchManager.GetAlbumsAsync(searchTerm)
+                .ConfigureAwait(false);
+
+            return query.Albums.Select(x =>
+                new Album(x.ArtistName, x.CollectionName,
+                    x.ArtworkUrl100.Replace("100x100bb", "600x600bb")));
+        }
+
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string CoverUrl { get; set; }
+    }
+}
---
+++ ViewModels/AlbumViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Media.Imaging;
+using Avalonia.MusicStore.Models;
+using ReactiveUI;
+using System.Threading.Tasks;
+
+namespace Avalonia.MusicStore.ViewModels
+{
+    public class AlbumViewModel : ViewModelBase
+    {
+
+        private readonly Album _album;
+
+        private Bitmap? _cover;
+
+
+        public AlbumViewModel(Album album)
+        {
+            _album = album;
+        }
+
+        public Bitmap? Cover
+        {
+            get => _cover;
+            private set => this.RaiseAndSetIfChanged(ref _cover, value);
+        }
+
+        public async Task LoadCover()
+        {
+            await using (var imageStream = await _album.LoadCoverBitmapAsync())
+            {
+                Cover = await Task.Run(() => Bitmap.DecodeToWidth(imageStream, 400));
+            }
+        }
+
+        public string Artist => _album.Artist;
+        public string Title => _album.Title;
+    }
+}
